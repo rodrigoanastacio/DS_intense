c572d27289b7a4ce0dc795a9493e50290d527f48
Rodrigo Anastacio <rodrigo.anastacio@wundermanthompson.com>
2022-05-07 18:43:35 -0300

    Config project

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
-export default function Home() {}
+export default function Home() {
+  return <h1 className={styles.container}>Home</h1>
+}
