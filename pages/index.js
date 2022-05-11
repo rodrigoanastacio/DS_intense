74c33b53419432c24050d33c54a5452c2dee79b8
Rodrigo Anastacio <rodrigo.anastacio@wundermanthompson.com>
2022-05-11 16:16:53 -0300

    Adjusted styles

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,19 @@ import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <Hero>
-      <Headline />
-      <div className={styles.img}>
-        <img src="dayane-silva.svg" alt="Dayane Silva" />
-      </div>
-    </Hero>
+    <>
+      <Hero>
+        <Headline />
+        <div className={styles.img}>
+          <img src="dayane-silva.svg" alt="Dayane Silva" />
+        </div>
+      </Hero>
+
+      <section>
+        <div className={styles.container}>
+          <h2>Lorem ipsum dolor sit amet.</h2>
+        </div>
+      </section>
+    </>
   );
 }
